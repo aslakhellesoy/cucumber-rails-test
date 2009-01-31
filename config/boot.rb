f9c38eef38e1f035106ea8e812a462df94200456
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-01-31 02:44:34 +0100

    Try to make app work on older rails

--- config/boot.rb
+++ config/boot.rb
@@ -43,7 +43,7 @@ module Rails
   class VendorBoot < Boot
     def load_initializer
       require "#{RAILS_ROOT}/vendor/rails/railties/lib/initializer"
-      Rails::Initializer.run(:install_gem_spec_stubs)
+      Rails::Initializer.run(:install_gem_spec_stubs) rescue nil
     end
   end
 
